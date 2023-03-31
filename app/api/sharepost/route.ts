45a864e1f393007b35f5b2d7c32c0832e4858d1a
Yannick Lml <yle-mouel@laposte.net>
2023-03-31 13:45:42 +0100

    commit after type share api route

--- app/api/sharepost/route.ts
+++ app/api/sharepost/route.ts
@@ -1,12 +1,19 @@
 import { prisma } from "@/prisma/prisma";
 import { NextResponse } from "next/server";
 
+type CreatePost ={
+    data: {
+    title: string,
+    imageUrl: string,
+    tag:string,
+    }
+}
 
 //  ---Permet de créer le post en base de donnée au moment du partage  ---
 export async function POST(request: Request) {
 	const { title, imageUrl, tag } = await request.json();
 
-	const data = await prisma.post.create({
+	const data = await prisma.post.create<CreatePost>({
         data: {
             title,
             imageUrl,
